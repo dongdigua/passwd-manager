1dcc1e67220c118ca8085d83e028f15cc04fd56b
dongdigua <dongdigua@outlook.com>
2022-06-20 12:03:20 +0800

    anyway, it works!

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,4 +1,5 @@
 use std::array;
+use std::ops::Rem;
 use std::str;
 use std::iter;
 use aes::cipher::block_padding::Padding;
@@ -6,7 +7,7 @@ use base64;
 
 use aes::Aes128;
 use aes::cipher::{
-    KeyIvInit, block_padding::Pkcs7, BlockDecryptMut, BlockEncryptMut,
+    KeyIvInit, block_padding::{Pkcs7, ZeroPadding}, BlockDecryptMut, BlockEncryptMut,
 };
 
 type Aes128CbcEnc = cbc::Encryptor<aes::Aes128>;
@@ -21,27 +22,27 @@ pub fn encrypt(text: String, key: [u8; 16]) -> String {
 
     buf[..text_len].copy_from_slice(text.as_bytes());
     let ct = Aes128CbcEnc::new(&key.into(), &IV.into())
-        .encrypt_padded_mut::<Pkcs7>(&mut buf, text_len)
+        .encrypt_padded_mut::<ZeroPadding>(&mut buf, text_len)
         .unwrap();
 
-    println!("{:?}", &ct);
+    //println!("{:?}", &ct);
     base64::encode(ct)
 }
 
 pub fn decrypt(encrypted: String, key: [u8; 16]) -> String {
     let mut encrypted_array = base64::decode(encrypted).unwrap();
-    println!("{:?}", encrypted_array);
-    let mut buf = [0u8; BUFFER_SIZE];
+    //println!("{:?}", encrypted_array);
     let text_len = encrypted_array.len();
+    let mut buf = [0u8; BUFFER_SIZE];
 
     buf[..text_len].copy_from_slice(&encrypted_array);
-    //Pkcs7::pad(block, pos)
+    //Pkcs7::pad(buf.into(), text_len);
 
     let pt = Aes128CbcDec::new(&key.into(), &IV.into())
-        .decrypt_padded_mut::<Pkcs7>(&mut buf)
+        .decrypt_padded_mut::<ZeroPadding>(&mut buf)
         .unwrap();
 
-    unsafe {str::from_utf8_unchecked(pt).to_string()}
+    unsafe {str::from_utf8_unchecked(&pt)[..text_len].replace("\0", "").to_string()}
 }
 #[cfg(test)]
 mod test {
@@ -56,6 +57,8 @@ mod test {
     fn able_to_decrypt_to_original() {
         let original = "yee".to_string();
         let key = [1u8; 16];
-        println!("{}", decrypt(encrypt(original, key), key))
+        let after = decrypt(encrypt(original.clone(), key), key);
+        //println!("{:?}", after)
+        assert_eq!(original, after)
     }
 }
